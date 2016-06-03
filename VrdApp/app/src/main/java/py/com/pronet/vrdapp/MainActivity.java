842c34c9f8ad71e3dd800e5d9c430bbf22cbc635
violeta <marizavioleta@gmail.com>
2016-06-03 10:51:33 -0400

    Initial commit 03/06/2016

---
+++ VrdApp/app/src/main/java/py/com/pronet/vrdapp/MainActivity.java
@@ -0,0 +1,13 @@
+package py.com.pronet.vrdapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
